7ab7803a0ec7e34d415142d52dff777ae9067d27
Malikireddy <UmaMaheswari.Malikireddy@thi.de>
2023-05-10 11:18:38 +0200

    Bootstrap container

---
+++ src/forms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => { 
+  return (
+    <div className = "container bg-dark py-3 py-3">      
+    <h1 className="text-success">Bootstrap Form</h1>  
+    </div>
+  )
+}
+
+export default Form;
